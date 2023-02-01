12551a75ad97df4a6548b0b1f3e74177c30ddbfb
vamsi krishna <vamsi.bandaru.22.1@mountblue.tech>
2023-02-02 00:00:09 +0530

    added some more dummy data and rendered on the screeen

--- src/components/MainPage/LeftComponent.js
+++ src/components/MainPage/LeftComponent.js
@@ -1,8 +1,37 @@
 import React, { Component } from 'react'
 import { GROUP_DATA } from '../../redux/actions/DataType'
+import { connect } from 'react-redux';
 
 class LeftComponent extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      friends: []
+    }
+  }
+  componentDidMount() {
+    let Allfriends = this.props.groups.reduce((acc, group) => {
+      return acc.concat(group.friends)
+    }, [])
+
+    const removeDuplicates = (arr) => {
+
+      let unique = arr.reduce(function (acc, curr) {
+
+        if (!acc.includes(curr)) {
+          acc.push(curr);
+        }
+        return acc;
+      }, []);
+      return unique;
+    }
+    const friends = removeDuplicates(Allfriends)
+    this.setState({
+      friends
+    })
+  }
   render() {
+
     return (
       <div className='right-contianer p-3'>
         <div className='dashboard'>
@@ -27,11 +56,19 @@ class LeftComponent extends Component {
               add
             </div>
           </div>
-          {
-            /* <div className='sec-text-area'>
-              <p>You do not have any groups yet. <i className="fa-solid fa-circle-question"></i></p>
-            </div> */
-          }
+          <div className='sec-text-area'>
+            {
+              this.props.groups.map((group) => {
+                return (
+                  <li key={group.groupName}>
+                    <h6><i className="fa-solid fa-tag"></i>{group.groupName}</h6>
+                  </li>
+                )
+              })
+            }
+            {/* 
+            <p>You do not have any groups yet. <i className="fa-solid fa-circle-question"></i></p> */}
+          </div>
 
         </div>
         <div className='friends'>
@@ -43,7 +80,20 @@ class LeftComponent extends Component {
             </div>
           </div>
           <div className='sec-text-area'>
-            <p>You have not added any friends yet.</p>
+            {
+              this.state.friends.map((friend) => {
+                console.log('friend')
+                return (
+                  <li key={friend}>
+                    <h6><i className="fa fa-user"></i>{friend}</h6>
+                  </li>
+                )
+              })
+            }
+            {
+
+            }
+            {/* <p>You have not added any friends yet.</p> */}
           </div>
         </div>
       </div>
@@ -51,4 +101,19 @@ class LeftComponent extends Component {
   }
 }
 
-export default LeftComponent
+const mapStateToProps = (stateInStore) => {
+  return {
+    groups: stateInStore.DummyData.groups
+  }
+}
+
+const mapDispatchToProps = {
+  AddFormData: (payload) => {
+    return {
+      type: GROUP_DATA,
+      payload,
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(LeftComponent);
--- src/redux/reducers/DummyData.js
+++ src/redux/reducers/DummyData.js
@@ -1,40 +1,81 @@
 import { GROUP_DATA } from "../actions/DataType";
 
 const initialState = {
-   group: [
+   groups: [
       {
          groupName: 'Bangalore Trip',
          friends: [
             "Bob",
-            "joe",
+            "Jhon",
             "Smith"
          ],
-         totalAmount: 1500
+         totalAmount: 25000
       },
       {
          groupName: 'Saturday Night',
          friends: [
             "Bob",
-            "joe",
             "Smith"
+         ],
+         totalAmount: 6500
+      },
+      {
+         groupName: 'New Year Party',
+         friends: [
+            "Bob",
+            "Jhon",
+            "Smith",
+            "Rahul"
          ]
          ,
-         totalAmount: 1500
-      }
+         totalAmount: 20000
+      },
+      {
+         groupName: 'Vinodh Birthday Party',
+         friends: [
+            "Bob",
+            "Jhon",
+            "Smith",
+            "Pavan",
+            "Vinodh",
+            "Veeru"
+         ]
+         ,
+         totalAmount: 500
+      },
+      {
+         groupName: 'Room expenses',
+         friends: [
+            "Bob",
+            "Jhon",
+         ]
+         ,
+         totalAmount: 500
+      },
+      {
+         groupName: 'Avatar Movie',
+         friends: [
+            "Bob",
+            "Jhon",
+            "Pavan"
+         ]
+         ,
+         totalAmount: 2000
+      },
    ]
 }
 
 const DummyData = (state = initialState, action) => {
    console.log('groupdata:', action)
    switch (action.type) {
-      case GROUP_DATA: 
-      return {
-         ...state,
-         group: {
-            ...state.group,
-            ...action.payload
-         }
-      };
+      case GROUP_DATA:
+         return {
+            ...state,
+            groups: {
+               ...state.group,
+               ...action.payload
+            }
+         };
       default:
          return state;
    }
