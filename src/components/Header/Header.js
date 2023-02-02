606d6766921c979ef199f332dd73bc97fea7aac4
vamsi krishna <vamsi.bandaru.22.1@mountblue.tech>
2023-02-02 15:00:24 +0530

    changed router link for logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -12,9 +12,7 @@ class Header extends Component {
       return (
          <nav className="navbar header">
             <div className="container header-content">
-               <Link to="/signup">
-                  <img id="logo" src="https://assets.splitwise.com/assets/core/logo-wordmark-horizontal-white-short-c309b91b96261a8a993563bdadcf22a89f00ebb260f4f04fd814c2249a6e05d4.svg" />
-               </Link>
+               <img id="logo" src="https://assets.splitwise.com/assets/core/logo-wordmark-horizontal-white-short-c309b91b96261a8a993563bdadcf22a89f00ebb260f4f04fd814c2249a6e05d4.svg" />
                <div className="header-right">
                   <div className="dropdown">
 
