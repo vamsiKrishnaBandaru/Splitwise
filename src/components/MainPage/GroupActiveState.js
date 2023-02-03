fc6298df9b4e1ffc617c856cf82311628db6a81e
vamsi krishna <vamsi.bandaru.22.1@mountblue.tech>
2023-02-03 15:20:49 +0530

    created ListGroupCard file to show the status of transactions and how much left to pay

--- src/components/MainPage/GroupActiveState.js
+++ src/components/MainPage/GroupActiveState.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux';
 import { GROUP_DATA, SIGN_IN_USER_DATA } from '../../redux/actions/DataType'
+import ListGroupCard from './ListGroupCard';
 
 class GroupActiveState extends Component {
    constructor(props) {
@@ -9,6 +10,7 @@ class GroupActiveState extends Component {
          howSpent: null,
          totalAmount: null,
          members: null,
+         listActive: null,
       }
    }
 
@@ -19,12 +21,11 @@ class GroupActiveState extends Component {
          }
       })
 
-      let totalAmount = result[0].howSpent.reduce((sum, amount) => {
-         sum += amount.cost
-         return sum
-      }, 0)
-      
       if (result.length > 0) {
+         let totalAmount = result[0].howSpent.reduce((sum, amount) => {
+            sum += amount.cost
+            return sum
+         }, 0)
          this.setState({
             howSpent: result[0].howSpent,
             totalAmount: totalAmount,
@@ -50,36 +51,13 @@ class GroupActiveState extends Component {
                this.state.howSpent &&
                <div className='container'>
                   <ul className='list-group mt-2 mx-2'>
-
                      {
                         this.state.howSpent.map(data => {
                            return (
-                              <li key={data.message} className="list-group-item message-container">
-
-                                 <div className='row message-date'>
-                                    <div className='col-2'>
-                                       <p>FEB</p>
-                                       <p>02</p>
-                                    </div>
-                                    <div className='col'>
-                                       <h6>{data.message}</h6>
-                                    </div>
-                                 </div>
-
-                                 <div className='spent-status'>
-                                    <div>
-                                       <p>You Paid</p>
-                                       <strong>${data.cost}</strong>
-                                    </div>
-                                    <div className='lent'>
-                                       <p>You lent</p>
-                                       <strong>{`$${(
-                                          data.cost -
-                                          (data.cost / (this.state.members.length + 1)).toFixed(2))
-                                          }`}</strong>
-                                    </div>
-                                 </div>
-
+                              <li key={data.message} className="list-group-item message-container" >
+                                 <ListGroupCard data={data}
+                                    members={this.state.members}
+                                    totalAmount={this.state.totalAmount} />
                               </li>
                            )
                         })
---
+++ src/components/MainPage/ListGroupCard.js
@@ -0,0 +1,104 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux';
+import { GROUP_DATA, SIGN_IN_USER_DATA } from '../../redux/actions/DataType'
+
+class ListGroupCard extends Component {
+   constructor(props) {
+      super(props)
+      this.state = {
+         listActive: false,
+      }
+   }
+
+   addClassName = () => {
+      if (this.state.listActive) {
+         this.setState({
+            listActive: false
+         })
+      } else {
+         this.setState({
+            listActive: true
+         })
+      }
+   }
+
+   render() {
+      return (
+         <div className='list-box'>
+            <div className='list-data-container' onClick={() => {
+               this.addClassName()
+            }}>
+               <div className='row message-date'>
+                  <div className='col-2'>
+                     <p>FEB</p>
+                     <p>02</p>
+                  </div>
+                  <div className='col'>
+                     <h6>{this.props.data.message}</h6>
+                  </div>
+               </div>
+
+               <div className='spent-status'>
+                  <div>
+                     <p>You Paid</p>
+                     <strong>${this.props.data.cost}</strong>
+                  </div>
+                  <div className='lent'>
+                     <p>You lent</p>
+                     <strong>{`$${(
+                        this.props.data.cost -
+                        (this.props.data.cost / (this.props.members.length + 1)).toFixed(2))
+                        }`}</strong>
+                  </div>
+               </div>
+            </div>
+            <div className={!this.state.listActive ? "Show-list-info" : ""}>
+               <div className='statusOf-prices row'>
+                  <div className='status-left col'>
+                     <img src="https://s3.amazonaws.com/splitwise/uploads/user/default_avatars/avatar-ruby38-50px.png" />
+
+                     <stong> {
+                        this.props.user.name
+                     } </stong> paid <strong>${this.props.totalAmount}</strong> and <span className='text-secondary'> owes</span> <strong> ${(this.props.totalAmount / (this.props.members.length) + 1).toFixed(2)}</strong>
+                     {
+                        this.props.members.map((member, index) => {
+                           let link = `https://s3.amazonaws.com/splitwise/uploads/user/default_avatars/avatar-grey${index + 1}-100px.png`
+                           return (
+                              <div className='mt-4'>
+                                 <span>
+                                    <img src={link}></img></span><span>
+                                    <strong> {member}</strong> <span className='text-secondary'> owes</span> <strong> ${(this.props.totalAmount / (this.props.members.length) + 1).toFixed(2)}</strong>
+                                 </span>
+                              </div>
+                           )
+                        })
+                     }
+                  </div>
+                  <div className='status-right col'>
+                     <strong>Spending by category</strong>
+                     <p className='text-secondary'>{this.props.user.activeGroup}::{this.props.data.message}</p>
+                  </div>
+               </div>
+            </div>
+         </div>
+      )
+   }
+}
+
+const mapStateToProps = (stateInStore) => {
+   return {
+      groups: stateInStore.DummyData.groups,
+      user: stateInStore.userData.user
+   }
+}
+
+const mapDispatchToProps = {
+   AddFormData: (payload) => {
+      return {
+         type: SIGN_IN_USER_DATA,
+         payload,
+      }
+   }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ListGroupCard);
